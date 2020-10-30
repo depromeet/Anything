e875e8981673736388c6f3d1f0fa9620d78654d4
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-10-30 19:35:11 +0900

    init

---
+++ Anything/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Anything
+//
+//  Created by Soso on 2020/10/23.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
