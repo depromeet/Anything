170db0c72383c1b0825fce78b3afd0c2e9ef97b7
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-11-08 17:20:17 +0900

    랜덤 rotation 추가

--- Anything/Scene/Main/MainViewController.swift
+++ Anything/Scene/Main/MainViewController.swift
@@ -6,22 +6,40 @@
 //  Copyright © 2020 Soso. All rights reserved.
 //
 
+import RxGesture
+import RxSwift
 import SnapKit
 import UIKit
 
 class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(hex: 0x383838)
+        view.backgroundColor = UIColor(hex: 0x262626)
 
         let circularView = CircularView()
         view.addSubview(circularView)
         circularView.snp.makeConstraints {
             $0.center.equalToSuperview()
-            $0.width.height.equalTo(300)
+            $0.width.height.equalTo(320)
         }
+
+        let total = 500
+        circularView.rx.tapGesture()
+            .when(.recognized)
+            .flatMap { _ -> Observable<CGFloat> in
+                let random = CGFloat.random(in: 0 ... 360)
+                return Observable<Int>
+                    .interval(.milliseconds(10), scheduler: MainScheduler.instance).take(total)
+                    .map { value -> CGFloat in
+                        return Cubic.EaseOut(CGFloat(value), 0, 360 * 5 + random, CGFloat(total))
+                    }
+            }
+            .map { Int($0 / 72) % 5 }
+            .bind(to: circularView.rx.currentIndex)
+            .disposed(by: disposeBag)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
