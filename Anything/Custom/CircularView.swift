60aacdb65c53d6395e9f733bf2fcdeba1d42bc74
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-11-08 16:59:00 +0900

    CircularView roundedCorner 추가

--- Anything/Custom/CircularView.swift
+++ Anything/Custom/CircularView.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 Soso. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class CircularView: UIView {
@@ -29,14 +31,13 @@ class CircularView: UIView {
         let midY = bounds.midY
         let center = CGPoint(x: midX, y: midY)
 
-        let spacing: CGFloat = 14
-        let numberOfSlice = 6
+        let spacing: CGFloat = 35
+        let numberOfSlice = 5
         let sliceAngle = 360 / numberOfSlice
-        let start = 0 // -90 + sliceAngle / 2
+        let start = -90
         let end = start + 360
 
         for (index, angle) in stride(from: start, to: end, by: sliceAngle).enumerated() {
-            let alpha: CGFloat = index == currentIndex ? 1 : 0.3
             let angle = angle.radians
             let radius: CGFloat = (spacing / 2) / sin((sliceAngle / 2).radians)
             let x = center.x + radius * cos(angle)
@@ -45,24 +46,31 @@ class CircularView: UIView {
 
             let startAngle = angle - (sliceAngle / 2).radians
             let endAngle = startAngle + sliceAngle.radians
-            let radiusLight: CGFloat = 100
-            let radiusDark: CGFloat = 90
 
-            UIColor.red.withAlphaComponent(alpha).setFill()
+            let radiusDark: CGFloat = 110
 
-            let pathLight = UIBezierPath()
-            pathLight.move(to: point)
-            pathLight.addArc(withCenter: point, radius: radiusLight, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            pathLight.close()
-            pathLight.fill()
-
-            UIColor.orange.withAlphaComponent(alpha).setFill()
+            if index == currentIndex {
+                UIColor(hex: 0x3C3C3C).set()
+            } else {
+                UIColor(hex: 0x2D2D2D).set()
+            }
 
             let pathDark = UIBezierPath()
             pathDark.move(to: point)
             pathDark.addArc(withCenter: point, radius: radiusDark, startAngle: startAngle, endAngle: endAngle, clockwise: true)
             pathDark.close()
+            pathDark.lineJoinStyle = .round
+            pathDark.lineWidth = 25
+            pathDark.stroke()
             pathDark.fill()
+        }
+    }
+}
+
+extension Reactive where Base: CircularView {
+    var currentIndex: Binder<Int> {
+        return Binder(base) { view, currentIndex in
+            view.currentIndex = currentIndex
         }
     }
 }
